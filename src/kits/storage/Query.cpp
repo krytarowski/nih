3bbb942a4cc3aff600edce6b1d30d665ebed8ee8
Kamil Rytarowski <n54@gmx.com>
2017-02-03 05:21:14 +0100

    src: kits: storage: Temporarily disable unbuildable code in Query.cpp
    To be reviewed later.

--- src/kits/storage/Query.cpp
+++ src/kits/storage/Query.cpp
@@ -359,8 +359,10 @@ BQuery::GetNextRef(entry_ref* ref)
 			}
 		}
 		if (error == B_OK) {
+#if !defined(__NetBSD__)
 			ref->device = entry.d_pdev;
 			ref->directory = entry.d_pino;
+#endif
 			error = ref->set_name(entry.d_name);
 		}
 	}
